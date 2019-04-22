0ac659189cf6a41cf5cc2aee11d391e58756805c
Kish Siva <rodes88@msn.com>
2019-04-22 19:33:42 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+  
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  #reads
+  def job
+    @job
+  end
+ 
+  #writes
+  def job=(new_job)
+    @job = new_job
+  end
+  
+end
