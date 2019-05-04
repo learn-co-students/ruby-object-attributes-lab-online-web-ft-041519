bf5fca540b05957c756c29966304b3e8af45e5d9
Runa V <runavalora@gmail.com>
2019-05-04 22:09:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name
+    @name
+  end
+
+  def name=(name_of_dog)
+    @name = (name_of_dog)
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed_type)
+    @breed = (breed_type)
+  end
+
+end
